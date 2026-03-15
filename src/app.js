78090ced40afd5b9bb58e27b5517f83a156cca7e
Sahil Sahu <sahusahil553@gmail.com>
2026-03-15 22:54:01 +0530

    Initial backend project

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const multer = require('multer');
+const uploadFile = require('./services/storage.service');
+const postModel = require('./models/post.model');
+const PostModel = require('./models/post.model');
+
+const app = express();
+app.use(express.json());
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.post('/create-post', upload.single('image'), async (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+
+    const result = await uploadFile(req.file.buffer);
+
+    const Post = await PostModel.create({
+        image: result.url,
+        caption: req.body.caption,
+});
+ return res.status(201).json({
+        message: 'Post created successfully',
+         Post,
+    });
+
+});
+
+app.get('/posts', async (req, res) => {
+    const posts = await PostModel.find();
+        return res.status(200).json({
+        message: 'Posts fetched successfully',
+         posts,
+
+        });
+});
+
+
+
+module.exports = app;
---
+++ src/services/storage.service.js
@@ -0,0 +1,18 @@
+const Imagekit = require('@imagekit/nodejs');
+
+
+const imagekit = new Imagekit({
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+});
+
+async function uploadFile(buffer) {
+
+    const result = await imagekit.files.upload({
+    file : buffer.toString('base64'),
+    fileName : 'file.jpg',
+});
+    return result;
+
+}
+
+module.exports = uploadFile;
